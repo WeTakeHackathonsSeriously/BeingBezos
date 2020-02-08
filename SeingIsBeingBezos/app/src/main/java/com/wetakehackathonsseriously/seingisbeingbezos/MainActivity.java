382e38d22a60c3c804c524c4427da925fcc84868
Champ-Goblem <cameron_mcdermott@yahoo.co.uk>
2020-02-08 21:08:33 +0000

    Almost completely working, slight bug camera freezes while taking snapshot and doesnt resume

--- SeingIsBeingBezos/app/src/main/java/com/wetakehackathonsseriously/seingisbeingbezos/MainActivity.java
+++ SeingIsBeingBezos/app/src/main/java/com/wetakehackathonsseriously/seingisbeingbezos/MainActivity.java
@@ -1,14 +1,211 @@
 package com.wetakehackathonsseriously.seingisbeingbezos;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.*;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextureView cameraPreview;
+    private CameraDevice camera;
+    private CameraCaptureSession cameraCaptureSession;
+    private String cameraID = "";
+    private Size imageDimension;
+    private Surface surface;
+    private CameraManager cameraManager;
+    private ImageReader snapshotReader;
+    private Size imageSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        cameraPreview = (TextureView)findViewById(R.id.textureView);
+        cameraPreview.setSurfaceTextureListener(textureListener);
+
+        final Handler handler = new Handler();
+        final int delay = 5000; //milliseconds
+
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                //do something
+                handler.postDelayed(this, delay);
+                if (camera == null || cameraManager == null) return;
+
+                final ImageReader snapshotReader = ImageReader.newInstance(imageSize.getWidth(), imageSize.getHeight(), ImageFormat.JPEG, 1);
+                ImageReader.OnImageAvailableListener snapshotListener = new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader reader) {
+                        Image image = null;
+                        image = snapshotReader.acquireLatestImage();
+                        Log.i("Camera", String.valueOf(image.getHeight()));
+                    }
+                };
+                snapshotReader.setOnImageAvailableListener(snapshotListener, null);
+                try {
+                     camera.createCaptureSession(Arrays.asList(snapshotReader.getSurface()), new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            try {
+                                CaptureRequest.Builder snapshotCaptureRequest = camera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+                                snapshotCaptureRequest.addTarget(snapshotReader.getSurface());
+                                session.capture(snapshotCaptureRequest.build(), null, null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                            Log.e("Camera", "Failed to configure for snapshot");
+                        }
+                    }, null);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, delay);
+    }
+
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice cm) {
+            camera = cm;
+            configureCamera();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            camera.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            camera.close();
+            camera = null;
+        }
+    };
+
+    private void configureCamera() {
+        // prepare list of surfaces to be used in capture requests
+        List<Surface> sfl = new ArrayList<Surface>();
+        SurfaceTexture texture = cameraPreview.getSurfaceTexture();
+        assert texture != null;
+        texture.setDefaultBufferSize(imageDimension.getWidth(), imageDimension.getHeight());
+        surface = new Surface(texture);
+        sfl.add(surface); // surface for viewfinder preview
+        try {
+            Size[] sizes = cameraManager.getCameraCharacteristics(cameraID).get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
+            imageSize = sizes[0];
+        } catch (CameraAccessException e) {}
+        // configure camera with all the surfaces to be ever used
+        try {
+            camera.createCaptureSession(sfl,
+                    new CaptureSessionListener(), null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+//        mIsCameraConfigured = true;
+    }
+
+    private class CaptureSessionListener extends
+            CameraCaptureSession.StateCallback {
+        @Override
+        public void onConfigureFailed(final CameraCaptureSession session) {
+            Log.d("Camera", "CaptureSessionConfigure failed");
+        }
+
+        @Override
+        public void onConfigured(final CameraCaptureSession session) {
+            Log.d("Camera", "CaptureSessionConfigure onConfigured");
+            cameraCaptureSession = session;
+
+            try {
+                CaptureRequest.Builder previewRequestBuilder = camera
+                        .createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                previewRequestBuilder.addTarget(surface);
+                cameraCaptureSession.setRepeatingRequest(previewRequestBuilder.build(),
+                        null, null);
+            } catch (CameraAccessException e) {
+                Log.d("Camera", "setting up preview failed");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void openCamera() {
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        Log.e("Camera", "is camera open");
+        try {
+            cameraID = cameraManager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraID);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            assert map != null;
+            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+            // Add permission for camera and let user grant the permission
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+                return;
+            }
+            cameraManager.openCamera(cameraID, stateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        Log.e("Camera", "openCamera X");
+    }
+
+    private class CaptureCameraSnapshot extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
     }
 }
